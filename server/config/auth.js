00818aecb5b84a08ef409825de7cd633040a80f0
info@michaelbowlin.com <keplerdog!>
2016-03-24 14:01:25 -0600

    Added auth.js.

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,33 +1,25 @@
 var passport = require('passport');
 
-exports.authenticate = function(req, res, next) {
-    var auth = passport.authenticate('local', function(err, user) {
+
+// middleware that will authenticate user when someone logs in with POST
+// trying to login using an XHR post (not the typical way to use passport)
+exports.authenticate = function(req, res, next){
+
+    // local -> is the strategy we're using
+    var auth = passport.authenticate('local', function(err, user){
         if(err) {return next(err);}
+        // failed to authenticate the user
         if(!user) { res.send({success:false})}
+
+        // create a session (we have to tell passport to login the user since we're using XHR -> not the typical way)
         req.logIn(user, function(err) {
             if(err) {return next(err);}
+            // send to the client a JSON object saying we were able to login the user
+            // also send the user down
             res.send({success:true, user: user});
         })
     })
+
+    // need to call the auth function
     auth(req, res, next);
 };
-
-exports.requiresApiLogin = function(req, res, next) {
-    if(!req.isAuthenticated()) {
-        res.status(403);
-        res.end();
-    } else {
-        next();
-    }
-};
-
-exports.requiresRole = function(role) {
-    return function(req, res, next) {
-        if(!req.isAuthenticated() || req.user.roles.indexOf(role) === -1) {
-            res.status(403);
-            res.end();
-        } else {
-            next();
-        }
-    }
-}
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,4 @@
-var passport = require('passport');
+var auth = require('./auth');
 
 module.exports = function(app) {
 
@@ -8,28 +8,7 @@ module.exports = function(app) {
         res.render('../../public/app/' + req.params[0]);
     });
 
-    // middleware that will authenticate user when someone logs in with POST
-    // trying to login using an XHR post (not the typical way to use passport)
-    app.post('/login', function(req, res, next){
-
-        // local -> is the strategy we're using
-        var auth = passport.authenticate('local', function(err, user){
-            if(err) {return next(err);}
-            // failed to authenticate the user
-            if(!user) { res.send({success:false})}
-
-            // create a session (we have to tell passport to login the user since we're using XHR -> not the typical way)
-            req.logIn(user, function(err) {
-                if(err) {return next(err);}
-                // send to the client a JSON object saying we were able to login the user
-                // also send the user down
-                res.send({success:true, user: user});
-            })
-        })
-
-        // need to call the auth function
-        auth(req, res, next);
-    });
+    app.post('/login', auth.authenticate);
 
     // Index View:
     // this allows index for any route that is not built
