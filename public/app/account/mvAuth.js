ceb8ed4ff6fc3244a5850d864f69a00cdabc90ff
info@michaelbowlin.com <keplerdog!>
2016-03-24 13:53:32 -0600

    Added mvAuth service to take care of the business logic of post. Adde  library.

---
+++ public/app/account/mvAuth.js
@@ -0,0 +1,21 @@
+// adding in the q lib for a promise
+angular.module('app').factory('mvAuth', function($http, mvIdentity, $q){
+    return {
+        authenticateUser: function(username, password) {
+
+            // communicate back to the controller using a promise
+            var dfd = $q.defer();
+
+            $http.post('/login', {username:username, password:password}).then(function(response){
+                if(response.data.success) {
+                    mvIdentity.currentUser = response.data.user;
+                    dfd.resolve(true);
+                } else {
+                    dfd.resolve(false);
+                }
+            });
+            return dfd.promise;
+
+        }
+    }
+});
--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,14 +1,13 @@
-angular.module('app').controller('mvNavBarLoginCtrl',function($scope, $http, mvIdentity, mvNotifier){
+angular.module('app').controller('mvNavBarLoginCtrl',function($scope, $http, mvIdentity, mvNotifier, mvAuth){
 
     $scope.identity = mvIdentity;
     $scope.signin = function(username, password) {
-        $http.post('/login', {username:username, password:password}).then(function(response){
-          if(response.data.success) {
-              mvIdentity.currentUser = response.data.user;
-              mvNotifier.notify('You have logged in!');
-          } else {
-              mvNotifier.notify('Username/Password combination incorrect');
-          }
-        })
+        mvAuth.authenticateUser(username, password).then(function(success) {
+            if(success) {
+                mvNotifier.notify('You have logged in!');
+            } else {
+                mvNotifier.notify('Username/Password combination incorrect');
+            }
+        });
     }
 });
