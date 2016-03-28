c326566aa162cabb632463ae062bbd974c4f052b
info@michaelbowlin.com <keplerdog!>
2016-03-28 12:31:40 -0600

    Created home page redirect if non admin tries to go to admin/users page.

--- public/app/app.js
+++ public/app/app.js
@@ -9,8 +9,33 @@ angular.module('app').config(function($routeProvider, $locationProvider){
     // define routes
     $routeProvider
         .when('/', {
-            templateUrl: '/partials/main/main', controller: 'mvMainCtrl'
+            templateUrl: '/partials/main/main',
+            controller: 'mvMainCtrl'
+        })
+        .when('/admin/users', {
+            templateUrl: '/partials/admin/user-list',
+            controller: 'mvUserListCtrl',
+            // route resolver
+            resolve: {
+                auth: function(mvIdentity, $q) {
+                    if(mvIdentity.currentUser && mvIdentity.currentUser.roles.indexOf('admin') > -1) {
+                        return true;
+                    } else {
+                        return $q.reject('not authorized')
+                    }
+                }
+            }
         });
 
 
 });
+
+// Redirecting if non admin on api/users
+// by calling run the code inside is going to be configured after compilation
+angular.module('app').run(function($rootScope, $location){
+    $rootScope.$on('$routeChangeError', function(evt, current, previous, rejection) {
+        if(rejection === 'not authorized') {
+            $location.path('/');
+        }
+    })
+})
