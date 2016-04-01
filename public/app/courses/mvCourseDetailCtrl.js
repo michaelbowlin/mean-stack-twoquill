efe7e99378706b0c5eb0e7b7331f064f29529760
info@michaelbowlin.com <keplerdog!>
2016-04-01 09:31:22 -0600

    Cached page so that all the courses are called initially. So when a single course is clicked on the server is not called.

--- public/app/courses/mvCourseDetailCtrl.js
+++ public/app/courses/mvCourseDetailCtrl.js
@@ -1,5 +1,16 @@
-angular.module('app').controller('mvCourseDetailCtrl', function($scope, mvCourse, $routeParams) {
+angular.module('app').controller('mvCourseDetailCtrl', function($scope, mvCachedCourses, $routeParams) {
     // Client property -> id -> (mvCourseDetailCtrl.js)
     // Server property -> _id -> (mvCourse.js)
-    $scope.course = mvCourse.get({_id:$routeParams.id})
+    // $scope.course = mvCourse.get({_id:$routeParams.id})
+
+    // query returns an arry that exposes a promise resolves when the data comes back
+    // contained in the $promise property
+    mvCachedCourses.query().$promise.then(function(collection){
+        // iterate through collection, find course and put it on the scope
+        collection.forEach(function(course) {
+            if(course._id === $routeParams.id) {
+                $scope.course = course;
+            }
+        })
+    })
 });
