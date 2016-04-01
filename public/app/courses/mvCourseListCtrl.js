81ae2d24d50a2efcf1b2cffb6cde4d64933380b3
info@michaelbowlin.com <keplerdog!>
2016-04-01 05:43:54 -0600

    Added sorting and filtering

--- public/app/courses/mvCourseListCtrl.js
+++ public/app/courses/mvCourseListCtrl.js
@@ -1,4 +1,13 @@
 angular.module('app').controller('mvCourseListCtrl', function($scope, mvCourse) {
     // will need a RESOURCE to query
     $scope.courses = mvCourse.query();
+
+    // Sort options
+    $scope.sortOptions = [
+        {value:"title",text: "Sort by Title"},
+        {value:"published",text: "Sort by Publish Date"},
+    ];
+
+    // auto sort the list by first item: title in this case
+    $scope.sortOrder = $scope.sortOptions[0].value;
 });
